e69bec329100c51cd30bf3ef72eeb92db9fa5e73
sivanathan balasubramanian <sivanathanb@gmail.com>
2017-09-26 02:10:04 +0530

    for user authentication

---
+++ src/private/AirplusWCF/AirplusWcf/AirplusWcf/Security/AuthorizationPolicy.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.IdentityModel.Policy;
+using System.IdentityModel.Claims;
+using System.IdentityModel;
+using System.Security.Principal;
+
+namespace AirplusWcf.Security
+{
+    public class AuthorizationPolicy : IAuthorizationPolicy
+    {
+        public ClaimSet Issuer;
+
+        //public string Id ="";
+        Guid _id = Guid.NewGuid();
+        public string id;
+        public bool Evaluate(EvaluationContext evaluationContext, ref object state)
+        {
+            // get the authenticated client identity
+            IIdentity client = GetClientIdentity(evaluationContext);
+            // set the custom principal
+            evaluationContext.Properties["Principal"] = new CustomPrincipal(client);
+            return true;
+            
+        }
+        private IIdentity GetClientIdentity(EvaluationContext evaluationContext)
+        {
+            object obj;
+            if (!evaluationContext.Properties.TryGetValue("Identities", out obj))
+                throw new Exception("No Identity found");
+            IList<IIdentity> identities = obj as IList<IIdentity>;
+            if (identities == null || identities.Count <= 0)
+                throw new Exception("No Identity found");
+            return identities[0];
+        }
+    }
+}
---
+++ src/private/AirplusWCF/AirplusWcf/AirplusWcf/Security/CustomPrincipal.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Security.Principal;
+using System.Security;
+
+namespace AirplusWcf.Security
+{
+    public class CustomPrincipal : IPrincipal
+    {
+        public IIdentity Identity;
+
+        public CustomPrincipal(IIdentity client)
+        {
+
+        }
+
+        public bool IsInRole(string role)
+        {
+            return true;
+        }
+    }
+}
