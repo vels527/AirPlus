7e9df8915908c378caf744724f5b1b1c2d04445e
Sivanathan Balasubramanian <sivanathanb@gmail.com>
2017-10-05 15:37:50 +0530

    New class called Listings.cs added in mongodatalayer wcf

---
+++ src/private/AirplusWCF/AirplusWcf/MongoDataLayer/Listings.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB;
+using MongoDB.Driver;
+using MongoDB.Driver.Core;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson.Serialization.IdGenerators;
+
+namespace MongoDataLayer
+{
+    [BsonDiscriminator("Listings")]
+    public class Listings
+    {
+        [BsonElement("User")]
+        public UserData User { get; set; }
+
+
+    }
+}
