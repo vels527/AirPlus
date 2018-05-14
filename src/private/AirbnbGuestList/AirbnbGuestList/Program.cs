b22c06b4441b490b968e3a16ee6a80e157319a1e
sivanathanb04 <sivanathanb@gmail.com>
2018-05-14 14:10:30 +0530

    modified:   Program.cs

--- src/private/AirbnbGuestList/AirbnbGuestList/Program.cs
+++ src/private/AirbnbGuestList/AirbnbGuestList/Program.cs
@@ -25,35 +25,45 @@ namespace AirbnbGuestList
     public class Guest
     {
         string apiKey = Environment.GetEnvironmentVariable("SENDGRID_API_KEY");
+        public static int i = 0;
         SendGridClient client;
         public string FirstName { get; set; }
         public string FullName { get; set; }
         public DateTime StartDate { get; set; }
         public DateTime EndDate { get; set; }
         public string Message { get; set; }
+
         EmailAddress from = new EmailAddress("siva@kustotech.in", "siva");
-        string subject = "Morning Airplus Calendar Month";
-        EmailAddress to = new EmailAddress("saran@kustotech.in", "saran");
+        public string subject = "Reminder Check In - Check Out";
+        EmailAddress to = new EmailAddress("sivanathanb@gmail.com", "saran");
         EmailAddress cc = new EmailAddress("siva@kustotech.in", "siva");
         public Guest()
         {
             client = new SendGridClient(apiKey);
+            i++;
+            if (i > 2)
+            {
+                return;
+            }
         }
-        public bool SendMail()
+        public async Task SendMail()
         {
             var msg = MailHelper.CreateSingleEmail(from, to, subject, "Hi", Message);
             msg.AddCc(cc);
-            var response = client.SendEmailAsync(msg);
-            if (response.IsCompleted)
-            {
-                return true;
-            }
-            return false;
+            var response = await client.SendEmailAsync(msg);
+
         }
     }
     public class GuestList
     {
-        public List<Guest> Guests { get; set; } 
+        public List<Guest> Guests { get; set; }
+        public Guest checkOutGuest { get; set; }
+        public Guest checkInGuest { get; set; }
+        public GuestList()
+        {
+            checkInGuest = new Guest();
+            checkOutGuest = new Guest();
+        }
         public bool processJSON(string JSON)
         {
             DateTime today = DateTime.Now;
@@ -70,40 +80,49 @@ namespace AirbnbGuestList
                 {
                     string startDate = doc["reservation"]["start_date"];
                     string endDate = doc["reservation"]["end_date"];
-                    if (tomorrowStr == startDate)
+                    if (tomorrowStr == startDate && checkInGuest.Message is null)
                     {
                         Guest guest = new Guest();
-                        guest.FullName = doc["reservation"]["full_name"];
-                        guest.FirstName =doc["reservation"]["first_name"];
+                        guest.FullName = doc["reservation"]["guest"]["full_name"];
+                        guest.FirstName =doc["reservation"]["guest"]["first_name"];
                         guest.Message = "Send Check in Mail for " + guest.FullName + ".He is checking in on "+ tomorrowStr;
+                        guest.subject = "Check in Mail Reminder";
                         //guest.CheckOutMail = "";
-                        Guests.Add(guest);
+                        checkInGuest = guest;
                     }
-                    else if (tomorrowStr == endDate)
+                    else if (tomorrowStr == endDate && checkOutGuest.Message is null)
                     {
                         Guest guest = new Guest();
-                        guest.FullName = doc["reservation"]["full_name"];
-                        guest.FirstName = doc["reservation"]["first_name"];
+                        guest.FullName = doc["reservation"]["guest"]["full_name"];
+                        guest.FirstName = doc["reservation"]["guest"]["first_name"];
                         //guest.CheckInMail = "";
+                        guest.subject = "Check out Mail Reminder";
                         guest.Message = "Send Check out Mail for " + guest.FullName + ".He is checking out on " + tomorrowStr;
-                        Guests.Add(guest);
+                        checkOutGuest = guest;
                     }
-                    if (Guests.Count > 1)
+                    if (!(checkOutGuest.Message is null) && !(checkInGuest.Message is null))
                     {
                         break;
                     }
                 }
             }
-            if (Guests.Count < 1)
+            if (checkOutGuest.Message is null
+                && checkInGuest.Message is null)
             {
+                Guest guest = new Guest();
+                guest.FullName = "";
+                guest.FirstName = "";
+                guest.Message = "Today no checkout or check in mail.";
+                guest.SendMail().Wait();
                 return false;
             }
-            else
+            else if(!(checkInGuest.Message is null))
             {
-                foreach (Guest g in Guests)
-                {
-                    g.SendMail();
-                }
+                checkInGuest.SendMail().Wait();
+            }
+            else if (!(checkOutGuest.Message is null))
+            {
+                checkOutGuest.SendMail().Wait();
             }
             return true;
         }
