4084504b3e7c70fccb401665cca7b5708e38fb1e
sivanathanb04 <sivanathanb@gmail.com>
2018-06-03 17:26:46 +0530

    new file:   src/private/WebAirplus/WebAirplus/GuestInsert.cs

---
+++ src/private/WebAirplus/WebAirplus/GuestInsert.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAirplus
+{
+    public class GuestInsert
+    {
+        public int GuestId;
+        public int PropertyId;
+        public int HostId;
+        public int status;
+        public string Remarks;
+        public DateTime? RequestedCheckIn;
+        public DateTime? RequestedCheckOut; 
+        public DateTime? CheckOutCleaning;
+    }
+}
